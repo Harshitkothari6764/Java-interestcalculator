89dcec1bc0b656e482e45061e90ae4f9479f414e
Harshitkothari6764 <91771968+Harshitkothari6764@users.noreply.github.com>
2021-11-05 11:28:04 -0400

    Add files via upload

---
+++ interestCalculator/src/interestCalculator/calcualtorRunner.java
@@ -0,0 +1,11 @@
+package interestCalculator;
+import java.math.BigDecimal;
+public class calcualtorRunner {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println(calculator.value());
+	}
+
+}
